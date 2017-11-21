dd0e6e14c1816994d8015e0a89ae41e9065c2786
Jamie Sherriff <sherriffjamie@gmail.com>
2017-11-21 21:12:59 +1300

    Fix linting
    add fix lint task

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
+const lintFileDirList = ['**/*.js', '!node_modules/**', '!platform-tools/**', '!playing/**'];
+
 gulp.task('lint', () => {
 	// ESLint ignores files with "node_modules" paths.
 	// So, it's best to have gulp ignore the directory as well.
 	// Also, Be sure to return the stream from the task;
 	// Otherwise, the task may end before the stream has finished.
-	return gulp.src(['**/*.js', '!node_modules/**', '!platform-tools/**', '!playing/**'])
+	return gulp.src(lintFileDirList)
 	// eslint() attaches the lint output to the "eslint" property
 	// of the file object so it can be used by other modules.
-		.pipe(eslint())
+		.pipe(eslint({}))
 		// eslint.format() outputs the lint results to the console.
 		// Alternatively use eslint.formatEach() (see Docs).
 		.pipe(eslint.format())
@@ -20,6 +22,24 @@ gulp.task('lint', () => {
 		// lint error, return the stream and pipe to failAfterError last.
 		.pipe(eslint.failAfterError());
 });
+
+gulp.task('lint-fix', () => {
+	// ESLint ignores files with "node_modules" paths.
+	// So, it's best to have gulp ignore the directory as well.
+	// Also, Be sure to return the stream from the task;
+	// Otherwise, the task may end before the stream has finished.
+	return gulp.src(lintFileDirList)
+	// eslint() attaches the lint output to the "eslint" property
+	// of the file object so it can be used by other modules.
+		.pipe(eslint({fix:true}))
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failAfterError last.
+		.pipe(eslint.failAfterError());
+});
+
 
 gulp.task('default', ['lint'], function () {
 	// This will only run if the lint task is successful...
